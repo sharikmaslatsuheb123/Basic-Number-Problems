ecd62d884c35026aac52fa18aed4945810cf2b7e
Suheb <114206876+sharikmaslatsuheb123@users.noreply.github.com>
2023-11-01 16:27:40 +0530

    Add files via upload

---
+++ NumberPattern.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class NumberPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1; i<=5; i++)
+		{
+			int num=1;
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(num+" ");
+				num++;
+			}
+			System.out.println(" ");
+			
+		}
+
+	}
+
+}
